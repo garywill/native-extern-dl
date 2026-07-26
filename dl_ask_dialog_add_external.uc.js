62e7542a060d6d1e117919771ae10e6ee421798c
garywill <garywill@disroot.org>
2026-07-26 10:15:15 +0800

    rewrite all code for ff153

--- dl_ask_dialog_add_external.uc.js
+++ dl_ask_dialog_add_external.uc.js
@@ -4,11 +4,8 @@
  * Show automatically-started download progress
  * 
  * By garywill (https://garywill.github.io)
- * Tested on Firefox 140
+ * Tested on Firefox 153
  * 
- * Modified from:
- *   https://www.firefox.net.cn/read-121336-2  
- *     18
  */
 
 // ==UserScript==
@@ -16,21 +13,39 @@
 // ==/UserScript==
 
 
+"use strict";
+
 console.log("dl_ask_dialog_add_external.uc.js");
     
 (() => {
-    let config = {
-        extraAppName: "uGet",   // external downloader display name
-        extraAppPath: "/usr/bin/uget-gtk",  // path to external downloader executable file // NOTICE
-        // extraAppPath: "/usr/bin/xmessage",
-    };
-    
-    function extraAppParamsArrGenerator(app, dlurl, useragent, referer, cookie, postdata, proxy) {
+    // NOTE Define you custom external downloaders here
+    const UserExternalDlers = [
+        {
+            dlerName: "uget",
+            dlerDisplayName: "uGet", // External downloader display name
+            binPath: "/usr/bin/uget-gtk",  // AbsPath to external downloader executable file
+        },
+
+        // NOTE Below are tests
+        // {
+        //     dlerName: "kdialog",
+        //     dlerDisplayName: "TestWKdialog",
+        //     binPath: "/usr/bin/kdialog",
+        // },
+        // {
+        //     dlerName: "test_no",
+        //     dlerDisplayName: "TestNo",
+        //     binPath: "echo", // use a existing command but not abs path. Should throw error
+        // },
+
+    ];
+
+    function gen_external_dler_args(app, dlurl, useragent, referer, cookie, postdata, proxy) {
         // dlurl useragent  referer  cookie  postdata : string
         // proxy : bool
         let result_arr = [];
 
-        if (app == 'uget')
+        if (app == 'uget' || app == 'kdialog')
         {
             if (useragent)
                 result_arr = result_arr.concat([`--http-user-agent=${useragent}`]);
@@ -38,171 +53,226 @@ console.log("dl_ask_dialog_add_external.uc.js");
             if (referer)
                 result_arr = result_arr.concat([`--http-referer=${referer}`]);
 
+            // aria2c不支持cookie ?
+            // 只支持 --http-cookie-file 而data无效？
             if (cookie)
                 result_arr = result_arr.concat([`--http-cookie-data=${cookie}`]);
 
             if (proxy)
             {
+                const proxy_type = 2; // 2 : uget http proxy
+                const proxy_ip = '127.0.0.1';
+                const proxy_port = 7990;
+                result_arr = result_arr.concat([
+                    `--proxy-type=${proxy_type}`,
+                    `--proxy-host=${proxy_ip}`,
+                    `--proxy-port=${proxy_port}`
+
+                ]);
             }
 
+            // aria2c不支持postdata ?
+            // 只支持 --http-post-file 而data无效？
             if (postdata)
                 result_arr = result_arr.concat([`--http-post-data=${postdata}`]);
 
             result_arr = result_arr.concat([dlurl]);
         }
+        else
+        {
+            let errmsg = `Unknown app '${app}'`;
+            // console.error(errmsg);
+            throw new Error(errmsg);
+        }
+
+        if (app == 'kdialog')
+            result_arr = ['--msgbox', 'ADDITIONAL_CHARS_TEST_1234abcd中文`~!@#$%^&*()_+=-";:,./<>?|[]{} ' + result_arr.toString() ];
+
         return result_arr;
     }
 
-    const dialogElement = document.documentElement.getButton ?
-                            document.documentElement : 
-                            document.getElementById('unknownContentType');
-    
-    
-    var MDownloadPlus = {
-        showAutoDlInfo: function() {
-            let hbox = document.createXULElement("div");
-            hbox.style.display = "grid";
-            
-            let filepath_box = document.createElement("input");
-            filepath_box.readOnly = true;
-            filepath_box.style.background = "none";
-            filepath_box.style.borderWidth = 0;
-            filepath_box.style.outline = 0;
-            filepath_box.style.margin = 0;
-            filepath_box.style.padding = 0;
-            filepath_box.style.whiteSpace = "nowrap";
-            filepath_box.style.fontSize = "7px";
-            
-            let info = document.createXULElement("div");
-            info.style.display = "block";
-            info.style.position = "relative";
-            info.style.fontSize = "10px";
-            
-            let info_text = document.createXULElement("div");
-            info_text.style.whiteSpace = "nowrap";
-            
-            
-            var progress = document.createXULElement("div");
-            progress.style.width = "1%";
-            progress.style.backgroundColor = "#0000ff1c";
-            progress.style.position = "absolute";
-            progress.style.display = "block";
-            progress.style.top = 0;
-            progress.style.left = 0;
-            progress.textContent = " 　 ";
-            
-            info.appendChild(info_text);
-            info.appendChild(progress);
-            hbox.appendChild(filepath_box);
-            hbox.appendChild(info);
-            
-            window.setInterval(function() {
-                var targetFile = dialog.mLauncher.targetFile;
-                
-                var filepath = targetFile.path ? targetFile.path : targetFile.persistentDescriptor;
-                var downloaded_size = targetFile.fileSize !== undefined ? targetFile.fileSize : targetFile.fileSizeOfLink;
-                var networkFile_size = dialog.mLauncher.contentLength; 
-                var space_avai = targetFile.diskSpaceAvailable;
-                
-                filepath_box.value = filepath;
-                
-                var percent; 
-                var disp_networkFile_size ;
-                if (networkFile_size > 0)
-                {
-                    disp_networkFile_size = memoryAddUnit(networkFile_size);
-                    percent = Math.floor(downloaded_size/dialog.mLauncher.contentLength * 100);
-                    progress.style.width = percent+"%";
-                }else {
-                    disp_networkFile_size = '?'; 
-                    percent = '?'; 
-                }
-                
-                info_text.textContent = `${percent}% ${memoryAddUnit(downloaded_size)} / ${disp_networkFile_size}  (${convertToSuperscript(downloaded_size)} / ${convertToSuperscript(dialog.mLauncher.contentLength)}) Space: ${memoryAddUnit(space_avai)}`
-                
-            }, 300);
-            
-            dialogElement.appendChild(hbox);
-        },
-        createExtraAppButton:function () {
-            let btn = dialogElement.getButton("extra2");
-            if(btn){
-                btn.setAttribute("label", config.extraAppName);
-                btn.addEventListener("click", function() { 
-                    var r = window.MDownloadPlus.lauchExtraApp(); 
-                    if (r)
-                        dialogElement.getButton("cancel").click();
-                    else
-                        alert("Something was wrong when tried to launch extra doenload app");
-                });
-                btn.setAttribute("hidden", "false");
-            }
-        },
-        lauchExtraApp:function () {
-            let url = dialog.mLauncher.source.spec;
-            
-            let regEx = new RegExp("^data:");
-            if (regEx.test(url)) {
-                alert("This link doesn't support external downloader");
-                return false;
-            }
-            
-            
-            let extraApp = Components.classes["@mozilla.org/file/local;1"]
-                            .createInstance(Components.interfaces.nsIFile);
-            extraApp.initWithPath(config.extraAppPath);
-            if (!extraApp.exists()) {
-                alert(config.extraAppName+ "Can not find " + config.extraAppPath);
-                return false;
-            }
-            
-            let ua_use = window.navigator.userAgent;
-            let commandArgs = extraAppParamsArrGenerator('uget', url, ua_use, null, null, null, false);
-            
-            let p = Components.classes["@mozilla.org/process/util;1"]
-                    .createInstance(Components.interfaces.nsIProcess);
-            p.init(extraApp);
-            p.run(false, commandArgs, commandArgs.length);
-            
-            return true;
-//             dialog.mDialog.dialog = null;
-//             window.close();
-        },
+    // ===============================================================
 
 
-        setDbClickUrlCopy:function () {
-            var element = document.querySelector("#source");
-            //element.value = dialog.mSourcePath;
-            //element.setAttribute("crop", "center");
-            window.setInterval( function() {
-                element.setAttribute("tooltiptext", dialog.mLauncher.source.spec); 
-            }, 500);
-            element.addEventListener("dblclick", function() {
-                Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(dialog.mLauncher.source.spec) ;
-            });
-        },
-        init:function () {
-            
-            this.setDbClickUrlCopy();
-            
-            this.createExtraAppButton();
-            
-            this.showAutoDlInfo();
-        }
-    }
-        
+    const url = dialog.mLauncher.source.spec;
+    const networkFile_size = dialog.mLauncher.contentLength;
+    const dialogElement = document.getElementById('unknownContentType');
+
     if (location.href.startsWith("chrome://mozapps/content/downloads/unknownContentType.x")) {
         if (UC_API)
-            UC_API.Runtime.startupFinished().then(start);
+            UC_API.Runtime.startupFinished().then(main);
         else
-            start();
+            main();
+    }
 
-        async function start() {
-            MDownloadPlus.init();
-            window.MDownloadPlus = MDownloadPlus;
+    function main()
+    {
+
+        enhanceUrlDisplay();
+
+        if (is_url_valid(url))
+        {
+            showAutoDlInfo(dialogElement);
+
+            createExternalDlBtns(dialogElement);
         }
     }
+
+    function is_url_valid(url)
+    {
+        let url2 = url.toLowerCase()
+        if (   url2.startsWith('http://')
+            || url2.startsWith('https://')
+            || url2.startsWith('ws://')
+            || url2.startsWith('wss://')
+            || url2.startsWith('ftp://')
+            || url2.startsWith('ftps://')
+            || url2.startsWith('sftp://')
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    function createExternalDlBtns(pElement) {
+        for (let dlerConfig of UserExternalDlers )
+        {
+            let btn = document.createXULElement('button');
+
+            btn.setAttribute("label", dlerConfig.dlerDisplayName);
+            btn.setAttribute("downloaderName", dlerConfig.dlerName);
+
+            btn.addEventListener("command", function() {
+                try {
+                    if ( runSystemExternalDler(dlerConfig) === true)
+                    {
+                        pElement.getButton("cancel").click();
+                        // dialog.mDialog.dialog = null;
+                        // window.close();
+                    }
+                }catch(err) {
+                    alert("Something was wrong when tring to launch external doenload app:\n" + err.message);
+                    console.error(err.message);
+                    console.error(err.toString());
+                    console.error(err.stack);
+                }
+            });
+            pElement.appendChild(btn);
+        }
+
+    }
+
+    function runSystemExternalDler (config) {
+        // console.log(config)
+        const nsIFile = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsIFile);
+
+        nsIFile.initWithPath(config.binPath);
+        if (!nsIFile.exists()) {
+            let errmsg = `Can not find bin ${config.binPath}`;
+            // console.error(errmsg)
+            throw new Error(errmsg)
+        }
+
+        let ua_use = window.navigator.userAgent;
+        let commandArgs = gen_external_dler_args(config.dlerName, url, ua_use, null, null, null, false);
+        // console.log(commandArgs)
+
+        let proc_util = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+        proc_util.init(nsIFile);
+        proc_util.run(false, commandArgs, commandArgs.length);
+
+        return true;
+    }
+
+
+
+
+    function showAutoDlInfo(pElement) {
+        let hbox = document.createXULElement("div");
+        hbox.style.display = "grid";
+
+        let tmpFilePathBox = document.createElement("input");
+        tmpFilePathBox.readOnly = true;
+        tmpFilePathBox.style.background = "none";
+        tmpFilePathBox.style.borderWidth = 0;
+        tmpFilePathBox.style.outline = 0;
+        tmpFilePathBox.style.margin = 0;
+        tmpFilePathBox.style.padding = 0;
+        tmpFilePathBox.style.whiteSpace = "nowrap";
+        tmpFilePathBox.style.fontSize = "7px";
+
+        let info = document.createXULElement("div");
+        info.style.display = "block";
+        info.style.position = "relative";
+        info.style.fontSize = "10px";
+
+        let info_text = document.createXULElement("div");
+        info_text.style.whiteSpace = "nowrap";
+
+
+        var progress = document.createXULElement("div");
+        progress.style.width = "1%";
+        progress.style.backgroundColor = "#0000ff1c";
+        progress.style.position = "absolute";
+        progress.style.display = "block";
+        progress.style.top = 0;
+        progress.style.left = 0;
+        progress.textContent = " 　 ";
+
+        info.appendChild(info_text);
+        info.appendChild(progress);
+        hbox.appendChild(tmpFilePathBox);
+        hbox.appendChild(info);
+
+        window.setInterval(function() {
+            var tmpFile = dialog.mLauncher.targetFile;
+
+            var tmpFilePath = tmpFile.path ? tmpFile.path : tmpFile.persistentDescriptor;
+            var downloaded_size = tmpFile.fileSize !== undefined ? tmpFile.fileSize : tmpFile.fileSizeOfLink;
+            var space_avai = tmpFile.diskSpaceAvailable;
+
+
+            tmpFilePathBox.value = tmpFilePath;
+
+            var percent;
+            var disp_networkFile_size ;
+            if (networkFile_size > 0)
+            {
+                disp_networkFile_size = memoryAddUnit(networkFile_size);
+                percent = Math.floor(downloaded_size/dialog.mLauncher.contentLength * 100);
+                progress.style.width = percent+"%";
+            }else {
+                disp_networkFile_size = '?';
+                percent = '?';
+            }
+
+            info_text.textContent = `${percent}% ${memoryAddUnit(downloaded_size)} / ${disp_networkFile_size}  (${convertToSuperscript(downloaded_size)} / ${convertToSuperscript(dialog.mLauncher.contentLength)}) Space: ${memoryAddUnit(space_avai)}`
+
+        }, 300);
+
+        pElement.appendChild(hbox);
+    }
+
+    function enhanceUrlDisplay() {
+        var element = document.querySelector("#source");
+        window.setInterval( function() {
+            element.setAttribute("tooltiptext",
+                                 dialog.mLauncher.source.spec + '\n'
+                                 + '(Double click to copy URL)'
+            );
+            element.style.textDecoration = 'underline';
+            element.style.cursor = 'pointer';
+        }, 500);
+        element.addEventListener("dblclick", function() {
+            Components.classes["@mozilla.org/widget/clipboardhelper;1"].getService(Components.interfaces.nsIClipboardHelper).copyString(dialog.mLauncher.source.spec) ;
+        });
+    }
+
+
     
+ 
+
+
     function memoryAddUnit(memory) {
         let unit = "";
         let mem_united = "?";
@@ -221,8 +291,10 @@ console.log("dl_ask_dialog_add_external.uc.js");
         }
         return mem_united;
     }
+
     function convertToSuperscript(str) {  
         str = str.toString();
+        // 创建一个映射表，将ASCII数字字符映射到上标字符（这里直接使用字符）  
         const superscriptMap = {  
             '0': '⁰',  
             '1': '¹',  
@@ -247,4 +319,7 @@ console.log("dl_ask_dialog_add_external.uc.js");
         
         return result;  
     }  
+
 })();
+
+
